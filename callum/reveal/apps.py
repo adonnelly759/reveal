beae6274061fe40deceb1e9b57fc457e89c40506
Aidan <adonnelly759@qub.ac.uk>
2019-12-24 15:41:28 +0000

    3rd

--- callum/reveal/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RevealConfig(AppConfig):
-    name = 'reveal'
--- callum/reveal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-
-
-app_name = "reveal"
-urlpatterns = [
-    path('', views.index, name='index'),
-]
